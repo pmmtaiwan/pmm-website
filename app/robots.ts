9c8bfff3b2174b59c68d5477233e26888786392b
pmmtaiwan <quartetchiang@gmail.com>
2026-07-21 16:43:07 +0800

    Align robots with canonical www domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://pmmtaiwan.org";
+const siteUrl = "https://www.pmmtaiwan.org";
 
 export default function robots(): MetadataRoute.Robots {
   return {
